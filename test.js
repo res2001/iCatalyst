a34990560617140e10906cfa4a5c30740ebd8a44
res2001 <res69@yandex.ru>
2015-06-15 10:43:55 +0300

    Окно выбора папки на JS
    Окно выбора папки на JS

---
+++ test.js
@@ -0,0 +1,8 @@
+var objShell = new ActiveXObject("shell.application");
+var ssfWINDOWS = 36;
+var objFolder;
+objFolder = objShell.BrowseForFolder(0, "Выберите каталог сохранения изображений.  Нажмите 'Отмена' для замены оригинальных изображений на оптимизированные.", ssfWINDOWS);
+if (objFolder != null)
+{
+	WScript.echo(objFolder.self.Path);
+}
